6f807dfa26f5cb4618afab352bef176e243ca48b
Blackout003 <blackout00003@gmail.com>
2025-09-02 18:33:45 +0200

    Remove Head component from layout and retain analytics script for improved performance.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,9 +28,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-          <Head>
             <script defer src="https://statistique.ganecloud.fr/script.js" data-website-id="e9526dfa-2a8c-47d6-be8c-7f67b581ef80"></script>
-          </Head>
         {children}
       </body>
     </html>
